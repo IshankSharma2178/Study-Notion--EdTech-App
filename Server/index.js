b00f2fd0c018491cf3d4f1375759fd5c3127b4fb
IshankSharma2178 <ishank.sharma_cs23@gla.ac.in>
2024-08-02 21:41:17 +0530

    Some Changes

--- Server/index.js
+++ Server/index.js
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials:true,
-    })
+	cors({
+		origin: "*",
+		credentials: true,
+	})
 )
 
 app.use(
