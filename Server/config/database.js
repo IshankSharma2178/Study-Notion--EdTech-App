fdea6719d43a9b988a922ed11e42819a599fd6dc
IshankSharma2178 <ishank.sharma_cs23@gla.ac.in>
2024-08-03 21:33:22 +0530

    Some Changes

--- Server/config/database.js
+++ Server/config/database.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 exports.connect=()=>{
     mongoose.connect(process.env.DATABASE_URL)
     .then(()=>{console.log('Connect to database successfully')})
-    .catch(err=>{console.log(`Error connecting to database ${process.env.DATABASE_URL}`);
+    .catch(err=>{console.log('Error connecting to database');
         process.exit(1);
     });
 }
