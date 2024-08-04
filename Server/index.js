b59e1b786669f363565eb177ffcf5fb67ccb604e
IshankSharma2178 <ishank.sharma_cs23@gla.ac.in>
2024-08-04 08:16:36 +0530

    Some Changes

--- Server/index.js
+++ Server/index.js
@@ -17,21 +17,12 @@ const PORT =process.env.PORT || 4000;
 app.use(express.json());
 app.use(cookieParser());
 
-const allowedOrigins = ['https://study-notion-version1.netlify.app'];
 
 app.use(cors({
-  origin: function(origin, callback){
-    // Allow requests with no origin like mobile apps or curl requests
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+    origin: 'https://study-notion-version1.netlify.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }));
 
 app.use(
     fileUpload({
