cc32a4044cc940d06e39b266763fd913e6888117
IshankSharma2178 <ishank.sharma_cs23@gla.ac.in>
2024-08-11 13:20:20 +0530

    Some changes in Cors

--- Server/index.js
+++ Server/index.js
@@ -18,12 +18,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials:true,
-    })
-)
+app.use(cors({
+    origin: 'https://main--study-notion-app-v1.netlify.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }));
 
 app.use(
     fileUpload({
